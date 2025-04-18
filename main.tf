32af61d81fd10f554a01b6984954c33762e34cf0
shirdheen <shirdheen2252@gmail.com>
2025-04-18 10:50:21 +1000

    Fix Terraform formatting

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 3.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
   }
 }
@@ -15,23 +15,23 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "rg" {
-    name = var.resource_group_name
-    location = var.location
+  name     = var.resource_group_name
+  location = var.location
 }
 
 # Creates a resource group, which is a logical container for our Azure resources. It allows us to group and manage resources as a single entity, simplifying deployment, access control and cleanup
 
 resource "azurerm_storage_account" "storage" {
-    name = var.storage_account_name
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    account_tier = "Standard" # Performance level (Standard or Premium)
-    account_replication_type = "LRS" # Redundancy level (LRS, GRS, etc.)
-    # LRS - 3 copies of your data in a single data centre within a region
-    # DOCS: https://learn.microsoft.com/en-us/azure/storage/common/storage-redundancy 
-    static_website {
-        index_document = "index.html"
-    } # Enables static website hosting with index.html as the homepage
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard" # Performance level (Standard or Premium)
+  account_replication_type = "LRS"      # Redundancy level (LRS, GRS, etc.)
+  # LRS - 3 copies of your data in a single data centre within a region
+  # DOCS: https://learn.microsoft.com/en-us/azure/storage/common/storage-redundancy 
+  static_website {
+    index_document = "index.html"
+  } # Enables static website hosting with index.html as the homepage
 }
 
 # Creates a storage account in the resource group. It is like a cloud-based hard drive on Azure. Gives us access to secure, scalable, and redundant storage for all kinds of data, such as files (images, videos, docs), backup and logs, application data, website assets
@@ -39,12 +39,12 @@ resource "azurerm_storage_account" "storage" {
 
 
 resource "azurerm_storage_blob" "index_html" {
-    name = "index.html"
-    storage_account_name = azurerm_storage_account.storage.name
-    storage_container_name = "$web"
-    type = "Block" # Defines the blob type
-    source = "${path.module}/website/index.html" # Path to the local file we are uploading
-    content_type = "text/html" # Used by brosers to interpret the file
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = "$web"
+  type                   = "Block"                             # Defines the blob type
+  source                 = "${path.module}/website/index.html" # Path to the local file we are uploading
+  content_type           = "text/html"                         # Used by brosers to interpret the file
 
 }
 
--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 # Using Terraform input variables, which are a powerful way to parameterize infrastructure code (can be reused with different values)
 
 variable "resource_group_name" {
-    default = "tf-static-site-rg" # If no value is passed at runtime, Terraform will use the default value
+  default = "tf-static-site-rg" # If no value is passed at runtime, Terraform will use the default value
 }
 
 variable "location" {
-    default = "Australia East" # Azure region
+  default = "Australia East" # Azure region
 }
 
 variable "storage_account_name" {
-    default = "tfstaticwebproject123"
+  default = "tfstaticwebproject123"
 }
